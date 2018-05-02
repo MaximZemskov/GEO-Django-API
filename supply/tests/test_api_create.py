2a552d2970b0f2b8f837836457b90f34cf6bf586
MaximZemskov <m.zemskov1@gmail.com>
2018-05-02 19:13:58 +0300

    add all tests for create suppleier api

--- supply/tests/test_api_create.py
+++ supply/tests/test_api_create.py
@@ -28,3 +28,61 @@ def test_api_create_supplier_unauthorized(client):
     }
     res = client.post('/api/suppliers/', data=data)
     assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_api_create_supplier_without_title(api_client):
+    data = {
+        "email": "test@example.com",
+        "phone_number": "+79999999999",
+        "address": "Street 5"
+    }
+    res = api_client.post('/api/suppliers/', data=data)
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {
+        "title": [
+            "This field is required."
+        ]
+    }
+
+
+@pytest.mark.django_db
+def test_api_create_supplier_with_empty_title(api_client):
+    data = {
+        "title": "",
+        "email": "test@example.com",
+        "phone_number": "+79999999999",
+        "address": "Street 5"
+    }
+    res = api_client.post('/api/suppliers/', data=data)
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {
+        "title": [
+            "This field may not be blank."
+        ],
+    }
+
+
+@pytest.mark.django_db
+def test_api_create_supplier_with_extra_field(api_client):
+    data = {
+        "title": "Test",
+        "email": "test@example.com",
+        "phone_number": "+79999999999",
+        "address": "Street 5",
+        "extra_field": 555,
+    }
+    res = api_client.post('/api/suppliers/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_api_create_supplier(api_client):
+    data = {
+        "title": "Test",
+        "email": "test@example.com",
+        "phone_number": "+79999999999",
+        "address": "Street 5",
+    }
+    res = api_client.post('/api/suppliers/', data=data)
+    assert res.status_code == status.HTTP_201_CREATED
