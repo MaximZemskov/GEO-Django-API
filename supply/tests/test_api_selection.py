4cf72c12ca8a482e8014b9abc87ff83c4675df6c
MaximZemskov <m.zemskov1@gmail.com>
2018-05-08 10:15:04 +0300

    add test to selection api

--- supply/tests/test_api_selection.py
+++ supply/tests/test_api_selection.py
@@ -85,7 +85,7 @@ def test_api_selection_with_few_poly_and_titles(api_client):
     y = 0
 
     test_poly_1 = Polygon(((-8, 7), (17, 7), (16, -18), (-15, -18), (-8, 7)))
-    test_poly_2 = Polygon(((-6., 8), (17, 7), (15, -11), (-12, -10), (-6, 8)))
+    test_poly_2 = Polygon(((-6, 8), (17, 7), (16, -18), (-15, -18), (-6, 8)))
 
     service_area_1 = ServiceAreaFactory.create(supplier=supplier_1,
                                                poly=test_poly_1)
@@ -131,3 +131,36 @@ def test_api_selection_without_query(api_client):
     assert res.status_code == status.HTTP_200_OK
     assert res.data['count'] == 0
     assert len(res.data['results']) == 0
+
+
+def test_api_selection_only_with_title(api_client):
+    supplier_1 = SupplierFactory.create()
+    supplier_2 = SupplierFactory.create()
+    supplier_3 = SupplierFactory.create()
+
+    x = 0
+    y = 0
+
+    test_poly_1 = Polygon(((-8, 7), (17, 7), (16, -18), (-15, -18), (-8, 7)))
+    test_poly_2 = Polygon(((-6, 8), (17, 7), (16, -18), (-15, -18), (-6, 8)))
+    test_poly_3 = Polygon(((-6, 9), (17, 7), (16, -18), (-15, -18), (-6, 9)))
+
+    service_area_1 = ServiceAreaFactory.create(supplier=supplier_1,
+                                               poly=test_poly_1)
+    service_area_2 = ServiceAreaFactory.create(supplier=supplier_2,
+                                               poly=test_poly_2)
+    service_area_3 = ServiceAreaFactory.create(supplier=supplier_3,
+                                               poly=test_poly_3)
+
+    service_1 = ServiceFactory.create(service_area=service_area_1)
+    ServiceFactory.create(service_area=service_area_2)
+    ServiceFactory.create(service_area=service_area_3)
+
+    test_title = service_1.title
+
+    res = api_client.get('/api/selection/?title={}'.format(test_title))
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data['count'] == 1
+    assert res.data['results'][0]['areas']['features'][0]['properties'][
+               'services'][0]['title'] == test_title
+
